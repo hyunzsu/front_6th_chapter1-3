b7fb439f6c2941948ccaaba38fa2b9eb0628369d
hyunzsu <hyunzsu@MacBook-Pro-3.local>
2025-07-24 04:45:15 +0900

    deepMemo 구현

--- packages/lib/src/hocs/deepMemo.ts
+++ packages/lib/src/hocs/deepMemo.ts
@@ -1,5 +1,12 @@
-import type { FunctionComponent } from "react";
+import { memo, type FunctionComponent } from "react";
+import { deepEquals } from "../equals";
 
+/**
+ * 컴포넌트의 props를 깊은 비교하여 불필요한 리렌더링을 방지하는 HOC입니다.
+ *
+ * @param Component - 메모이제이션할 함수형 컴포넌트
+ * @returns 메모이제이션된 컴포넌트
+ */
 export function deepMemo<P extends object>(Component: FunctionComponent<P>) {
-  return Component;
+  return memo(Component, deepEquals);
 }
