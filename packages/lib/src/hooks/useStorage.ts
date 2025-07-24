e18ebf8f948ee2a7aa2047120722a96f76bacf1a
hyunzsu <hyunzsu@MacBook-Pro-3.local>
2025-07-24 17:50:26 +0900

    useStorage 훅 구현

--- packages/lib/src/hooks/useStorage.ts
+++ packages/lib/src/hooks/useStorage.ts
@@ -3,7 +3,16 @@ import type { createStorage } from "../createStorage";
 
 type Storage<T> = ReturnType<typeof createStorage<T>>;
 
+/**
+ * storage의 상태를 구독하고 현재 값을 반환하는 Hook입니다.
+ *
+ * @template T storage에 저장되는 값의 타입
+ * @param storage createStorage로 생성된 storage 인스턴스
+ * @returns 현재 storage 값 (T | null)
+ */
 export const useStorage = <T>(storage: Storage<T>) => {
-  // useSyncExternalStore를 사용해서 storage의 상태를 구독하고 가져오는 훅을 구현해보세요.
-  return storage.get();
+  return useSyncExternalStore(
+    storage.subscribe, // 구독 함수
+    storage.get, // getSnapshot 함수
+  );
 };
